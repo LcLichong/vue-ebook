c396da3eeb9fb35f5785fceeee3271bb7847d890
LcLichong <736117210@qq.com>
2022-09-01 18:35:40 +0800

    全局样式

--- src/App.vue
+++ src/App.vue
@@ -15,15 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
 </script>
 
 <style>
-html,
-body {
-    height: 100vh;
-    margin: 0;
-}
 #app {
     height: 100%;
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
 }
 </style>
